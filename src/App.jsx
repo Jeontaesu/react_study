1a9938340e01bd11c7993fde7500354c084e4edc
Jeontaesu <jeontaesu@gmail.com>
2025-09-06 00:17:42 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div>
+			<h1 className="text-red-500">React Basic</h1>
+			<ul>
+				<li>list1</li>
+				<li>list2</li>
+				<li>list3</li>
+			</ul>
+		</div>
+	);
+}
+
+export default App;
